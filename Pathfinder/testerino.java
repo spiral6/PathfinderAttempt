1a3bcccdca11e31188f2e945ff049800274531b5
Shamee Mahmud <sami_safin_mahmud@hotmail.com>
2015-09-25 00:39:21 -0500

    First commit.

---
+++ Pathfinder/testerino.java
@@ -0,0 +1,13 @@
+public class testerino {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(printTest());
+
+	}
+	public static int printTest(){
+		System.out.print("Test");
+		return 1;
+	}
+
+}
